c5131e63574927dc20edf18dfb9c8bda78218099
Solomon <solomonmolla10@gmail.com>
2018-05-03 10:28:46 +0300

    Hyper parameters defined

--- network.py
+++ network.py
@@ -5,5 +5,9 @@ parameters = {
 	"BACH_SIZE": 100,
 	"HIDDEN_DIMS": 7,
 	"RECURRENT_LENGTH": 7,
-	""
+	"OUT_HIDDEN_DIMS": 32,
+	"OUT_RECURRENT_LENGTH": 2,
+
+	"LEARNING_RATE": 1e-3,
+	"EPOCH": 20,
 }
